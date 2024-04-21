b5b662e43b0ad38d66fd4d51ecf5e90b5f1288c2
Konvisar Karyna <konvisar_karina@knu.ua>
2024-04-21 14:49:33 +0300

    display road

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
 import pygame
 import pygame.freetype
 from src.car import MyCar
+from src.road import Road
+
+
+pygame.init()
 
 clock = pygame.time.Clock()
 
@@ -11,6 +15,11 @@ background_color = (0,0,0)
 #background_image = pygame.transform.scale(background_image, (500, 800))
 
 
+road_group = pygame.sprite.Group()
+spawn_road_time = pygame.USEREVENT
+pygame.time.set_timer(spawn_road_time, 5000)
+
+
 def get_car_image(filename, size, angle):
     image = pygame.image.load(filename)
     image = pygame.transform.scale(image, size)
@@ -18,19 +27,48 @@ def get_car_image(filename, size, angle):
     return image
 
 
-my_car_image = get_car_image('car1.png', size=(100, 70), angle=0)
+my_car_image = get_car_image('imgs/car1.png', size=(100, 70), angle=0)
+road_image = get_car_image('imgs/road1.png')
+road_image = pygame.transform.scale(road_image, (500, 400))
 
 
-car = MyCar((250,600), my_car_image)
+road = Road(road_image, (250,-600))
+road_group.add(road)
+road = Road(road_image, (250,-600))
+road_group.add(road)
+
+def spawn_road():
+    road = Road(road_image, (250,-600))
+    road_group.add(road)
+
+
+def draw_all():
+    road_group.update()
+    road_group.draw(screen)
+    car.draw(screen)
+
+
+
+
+
+
+
+car = MyCar((300,600), my_car_image)
+
+
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == spawn_road_time:
+            spawn_road()
+
     screen.fill(background_color)
-    car.draw(screen)
+    draw_all()
     pygame.display.flip()
     clock.tick(60)
+
 
 
 #def main():
