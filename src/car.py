d86d43b28d327c6552fd1ddf0194cfad15161bdf
Konvisar Karyna <konvisar_karina@knu.ua>
2024-04-21 17:18:04 +0300

    extending the move function

--- src/car.py
+++ src/car.py
@@ -7,22 +7,19 @@ class MyCar:
         self.rect = self.image.get_rect()
         self.rect.center = position
 
-
     def border(self):
         if self.rect.right > 480:
             self.rect.right = 480
         if self.rect.left < 110:
             self.rect.left = 110
 
-
     def move(self):
         key = pygame.key.get_pressed()
-        if key[pygame.K_a]:
-            self.rect.x -=4
-        elif key[pygame.K_d]:
+        if key[pygame.K_LEFT] or key[pygame.K_a]:
+            self.rect.x -= 4
+        elif key[pygame.K_RIGHT] or key[pygame.K_d]:
             self.rect.x += 4
         self.border()
-
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
